483e3dd082321442792ff72bf36ddae7a286528a
Ivnrmrz <63130819+Ivnrmrz@users.noreply.github.com>
2021-10-29 00:20:31 -0700

    Update task3.c

--- Comp 121/Homework/Week 8/For loops/task3.c
+++ Comp 121/Homework/Week 8/For loops/task3.c
@@ -1,17 +1,25 @@
-// Display all the unique prime factors for any positive number entered.
 
-// Have a current factor should count up starting at 2 in a for loop that stops at the original number entered.
+#include <stdio.h>
 
-// See if you can divide the entered number by the current factor by using the modulo operator.
-
-// When the number is divisible by the current factor, divide the number by that factor in a while loop that stops when 
-// the number is no longer divisible by the current factor.
-
-// Examples:
-
-// Please enter a number: 15
-// The unique prime factors are: 3 5
-// Please enter a number: 8
-// The unique prime factors are: 2
-
-
+int main()
+{
+    int i, ii, num, is_prime;
+    printf("Please enter a number:");
+    scanf("%d", &num);
+    printf("The unique prime factors are:");
+    for( i = 2 ; i <= num ; i++)
+    {  if( num % i == 0 )
+        {is_prime = 1;
+            for(ii = 2 ; ii <= i / 2; ii++)
+            { if( i % ii == 0)
+                { is_prime = 0;
+                 break;}
+            }
+            if( is_prime == 1 )
+            {
+                printf("%d, ", i);
+            }
+        }
+    }
+  return 0;
+}
