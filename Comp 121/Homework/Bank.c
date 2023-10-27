5003ab31149f4a3491906b79a40b4440e40dac05
Ivnrmrz <63130819+Ivnrmrz@users.noreply.github.com>
2023-10-27 14:26:04 -0700

    Create Bank.c

---
+++ Comp 121/Homework/Bank.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <unistd.h>
+
+int main (){
+	
+	double accountb = 123.45
+	char Username1 = "Ivan" ;
+	char Password1 = "password" ; 
+	char Username,Password;
+	
+	printf ("Enter your Username.")
+	scanf ("%c", &Username);
+	
+	printf ("Enter your Password.")
+	scanf ("%c", &Password);
+	
+	if  (Username1 == Username1){
+	
+		printf("Your account balance is %+d", accountb);
+	else (){
+		printf("Username and Password don't match account.")
+		
+		return(main) //check this 
+	}
+	}
+		
+	
+	
+	
+	
+	
+	
+	
+	
+	
+  struct Account {
+	char Username [36];
+	char Password [36];
+	double Balance [36];
+  };
+  struct Account
+	// Read the value from total.txt and display it as the amount of coins the user has.
+FILE * coinsf = fopen("total.txt", "r");
+  fscanf(coinsf, "%d", &coins);
+  printf("You currently have %d coins.\n", coins);
+}
+printf("That was everything in the file!\n");
+}
+
