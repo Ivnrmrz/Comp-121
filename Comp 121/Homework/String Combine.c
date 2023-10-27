172ad153182301081e4f361af403f587f0c39b75
Ivnrmrz <63130819+Ivnrmrz@users.noreply.github.com>
2023-10-27 09:37:01 -0700

    Update and rename Self.c to String Combine.c

--- Comp 121/Homework/String Combine.c
+++ Comp 121/Homework/String Combine.c
@@ -8,12 +8,14 @@ int main(){
 	char buffer[64];
 	
 	printf ("Enter FN.");
+	//stdin = standard input 
 	fgets(First, 32, stdin);
 	printf ("Enter LN.");
 	fgets(Last, 32, stdin);
 	
 	
 	strcpy (buffer, First);
+	//strccat adds onto string
 	strcat (buffer, Last);
 	
 	putf("Hello %c.",buffer);
