c6e435df96ab80c1912638369a5bf3f3690b6b8c
Ivnrmrz <63130819+Ivnrmrz@users.noreply.github.com>
2023-10-31 12:59:02 -0700

    Create Working with pointer arrays.c

---
+++ Comp 121/Homework/Linked in/Working with pointer arrays.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+	char *months[12] = {
+	"January", "Feburary","March","April","June","July","August","September","October","November","December"};
+	}
+	int x;
+	for(x=0,x<12,x++){
+		printf("%s\n", months[x]);
+		
+	return(0);
+	}
+}
